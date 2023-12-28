e8ab1f67aa9688327aff1c802ec3f41ff92ba9b1
fadilaMounira <mounira.langoul@facsciences-uy1.cm>
2023-12-28 18:01:15 +0100

    ajouter dans la basse de donne

--- Gestion_eleve/eleve/views.py
+++ Gestion_eleve/eleve/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .forms import enregistrementEleve
+from .models import User 
 
 # Create your views here.
 def add_show(request):
     if request.method == 'POST':
         fm = enregistrementEleve(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name = nm,email = em , password = pw)
+            reg.save()
+            fm = enregistrementEleve()
     else:
         fm = enregistrementEleve()
     return render(request, 'eleve/addandshow.html',{'form':fm})
